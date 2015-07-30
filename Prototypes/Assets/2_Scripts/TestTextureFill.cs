d799db411bf1f6f06f7dc03a49c234840e4812f7
louhichio <louhichio@hotmail.com>
2015-07-30 14:15:25 +0200

    Shaders 2

--- Prototypes/Assets/2_Scripts/TestTextureFill.cs
+++ Prototypes/Assets/2_Scripts/TestTextureFill.cs
@@ -10,20 +10,99 @@ public class TestTextureFill : MonoBehaviour {
 //		texture.Apply();
 //	}
 
-	private float radius = 2;	
-	void Start()
-	{		
-		transform.localScale = Vector3.one * radius;
-	}
+//	private float radius = 2;	
+//	void Start()
+//	{		
+//		transform.localScale = Vector3.one * radius;
+//	}
+//	
+//	void Update () 
+//	{	
+//		// get mouse pos
+//		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition); 
+//		RaycastHit hit ;
+//		if (Physics.Raycast (ray,out hit, Mathf.Infinity)) 
+//		{
+//			GetComponent<Renderer>().material.SetVector("_MousePos",new Vector4(hit.point.x,hit.point.y,hit.point.z,0));
+//		}
+//		if (Input.GetKey ("q"))
+//		{
+//			transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+//		}
+//		if (Input.GetKey ("d"))
+//		{
+//			transform.Rotate(new Vector3(0, -30, 0) * Time.deltaTime);
+//		}
+//	}
+	// the texture that i will paint with and the original texture (for saving)
+
+	public Texture2D sprayTexture, originalTexture ;
+	//temporary texture
+	private Texture2D tmpTexture;
+	// colors used to tint the first 3 mip levels
+	Color[] colors = new Color[3];
+	int mipCount ;
 	
-	void Update () 
-	{	
+	// tint each mip level
+
+	// actually apply all SetPixels, don't recalculate mip levels
+
+	void Start ()
+	{
+		//setting temp texture width and height 
+		tmpTexture = new Texture2D (originalTexture.width, originalTexture.height);
+		
+		colors[0] = Color.red;
+		colors[1] = Color.green;
+		colors[2] = Color.blue;
+		mipCount = Mathf.Min(3, tmpTexture.mipmapCount);
+		for( var mip = 0; mip < mipCount; ++mip ) {
+			var cols = tmpTexture.GetPixels( mip );
+			for( var i = 0; i < cols.Length; ++i ) {
+				cols[i] = Color.Lerp(cols[i], colors[mip], 0.33f);
+			}
+			tmpTexture.SetPixels( cols, mip );
+		}
+		//fill the new texture with the original one (to avoid "empty" pixels)
+		for (int y =0; y<tmpTexture.height; y++) {
+			for (int x = 0; x<tmpTexture.width; x++) {
+				tmpTexture.SetPixel (x, y, originalTexture.GetPixel (x, y));
+			}
+		}
+//		print (tmpTexture.height);
+		//filling a part of the temporary texture with the target texture 
+//		for (int y =0; y<tmpTexture.height; y++) 
+//		{
+//			for (int x = 0; x<tmpTexture.width; x++) 
+//			{
+//				if(y<tmpTexture.height-300)
+//				{
+//					tmpTexture.SetPixel (x, y, targetTexture.GetPixel (x, y));				
+//				}
+//				else
+//				tmpTexture.SetPixel(x,y,new Color(0,0,0,0));
+//			}
+//		}
+		//Apply 
+		tmpTexture.Apply (false);
+		//change the object main texture 
+		GetComponent<Renderer>().material.mainTexture = tmpTexture;
+	}
+
+	void Update()
+	{
 		// get mouse pos
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition); 
 		RaycastHit hit ;
 		if (Physics.Raycast (ray,out hit, Mathf.Infinity)) 
 		{
-			GetComponent<Renderer>().material.SetVector("_MousePos",new Vector4(hit.point.x,hit.point.y,hit.point.z,0));
+			print ("here" + hit.collider);
+			Vector2 pixelUV = hit.textureCoord;
+			pixelUV.x *= tmpTexture.width;
+			pixelUV.y *= tmpTexture.height;
+			tmpTexture.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.black);
+			tmpTexture.Apply();
+			GetComponent<Renderer>().material.mainTexture = tmpTexture;
 		}
 		if (Input.GetKey ("q"))
 		{
@@ -34,4 +113,5 @@ public class TestTextureFill : MonoBehaviour {
 			transform.Rotate(new Vector3(0, -30, 0) * Time.deltaTime);
 		}
 	}
+
 }
--- Prototypes/Assets/6_Shaders/DrawPainting.shader
+++ Prototypes/Assets/6_Shaders/DrawPainting.shader
@@ -4,7 +4,7 @@
 	{ 
 		_MainTex ("Texture", 2D) = "white" {} 
 		_Spray ("Spray Sprite", 2D) = "white" {} 
-		_PaintingSprite ("Texture", 2D) = "white" {} 
+		_PaintingSprite ("Painting Sprite", 2D) = "white" {} 
 		_MousePos ("ObjPos", Vector) = (-1,-10,-1,-1)
 		_Radius ("HoleRadius", Range(0.1,5)) = 2
 	}
@@ -22,8 +22,8 @@
 
 		CGPROGRAM
 		// add Alpha
-		#pragma surface surf Lambert alpha:blend finalcolor:finalblend
-
+		#pragma surface surf Lambert alpha:blend 
+//finalcolor:finalblend
 		sampler2D _MainTex; 	
 		sampler2D _PaintingSprite;
 		sampler2D _Spray;
@@ -42,13 +42,16 @@
 		void surf (Input IN, inout SurfaceOutput o) 
 		{ 
 		
-//			float dx = length(_ObjPos.x-IN.worldPos.x);
-//			float dy = length(_ObjPos.y-IN.worldPos.y);
-//			float dz = length(_ObjPos.z-IN.worldPos.z);
-//			float dist = (dx*dx+dy*dy+dz*dz) / _Radius;
+			float dx = length(_ObjPos.x-IN.worldPos.x);
+			float dy = length(_ObjPos.y-IN.worldPos.y);
+			float dz = length(_ObjPos.z-IN.worldPos.z);
+			float dist = (dx*dx+dy*dy+dz*dz) / _Radius;
 ////			float dist = distance(_ObjPos,IN.worldPos) / _Radius;
-//			dist = clamp(dist,0,1);
-//			
+			dist = clamp(dist,0,1);
+			if (dist <= 1)
+			{
+				tex2D(_MainTex, IN.uv_MainTex).rgb = tex2D(_PaintingSprite, IN.uv_PaintingSprite);
+			}
 //			fixed4 c = tex2D(_MainTex, IN.uv_MainTex) * dist; 
 //			
 //			o.Albedo = c.rgb;
@@ -58,19 +61,19 @@
 		
 		void finalblend (Input IN, SurfaceOutput o, inout fixed4 color)
 		{
-			float dx = length(_MousePos.x-IN.worldPos.x);
-			float dy = length(_MousePos.y-IN.worldPos.y);
-			float dz = length(_MousePos.z-IN.worldPos.z);
-			float dist = (dx*dx+dy*dy+dz*dz) / _Radius;
+//			float dx = length(_MousePos.x-IN.worldPos.x);
+//			float dy = length(_MousePos.y-IN.worldPos.y);
+//			float dz = length(_MousePos.z-IN.worldPos.z);
+//			float dist = (dx*dx+dy*dy+dz*dz) / _Radius;
 //			float dist = distance(_ObjPos,IN.worldPos) / _Radius;
-			if (dist <= 1)
-			{
-				color = tex2D(_PaintingSprite, IN.uv_PaintingSprite);
-			}
-			else
-			{
-				color = tex2D(_MainTex, IN.uv_MainTex);
-			}
+//			if (dist <= 1)
+//			{
+//				color = tex2D(_PaintingSprite, IN.uv_PaintingSprite);
+//			}
+//			else
+//			{
+//				color = tex2D(_MainTex, IN.uv_MainTex);
+//			}
 			
 //			else
 //			{
